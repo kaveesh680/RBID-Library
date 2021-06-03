3ddc0adad095682236f066de323e9ab10d6772f1
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-06-03 09:26:01 +0530

    Add dependencies and finish Welcome

---
+++ src/ClientApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Welcome from "./components/Welcome";
+import './assets/styles/main.scss';
+
+const ClientApp:React.FC = () => {
+    return(
+            <Welcome />
+    )
+}
+
+export default ClientApp;
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Row, Col, Container, Image} from "react-bootstrap";
+import libraryImg from '../assets/images/library.jpg';
+
+const Welcome:React.FC = () => {
+    return(
+        <Container fluid={true} className='welcome'>
+            <Row>
+                <Col xs={12} className='mt-2 mb-1'>
+                    <h1>My Library</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} className='mx-0 px-0'>
+                    <Image src={libraryImg} placeholder="Library"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={{span:2, offset:10}} className='mt-1 px-0' >
+                    <p>photo by Anna Hunko</p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Welcome;
